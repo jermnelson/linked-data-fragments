5a3bc9eaa0a47f527800aa094b9cb5297acca8b5
Jeremy Nelson <jermnelson.gmail.com>
2015-08-05 07:58:01 -0600

    Started REST api implemented with Falcon

--- api.py
+++ api.py
@@ -1,1 +1,13 @@
 __author__ = "Jeremy Nelson"
+import falcon
+
+rest = falcon.API()
+
+class Triple:
+
+    def on_get(self, req, resp, subj, pred, obj):
+        resp.body = None
+        
+
+rest.add_route('/<subj>/<pred>/<obj>',
+    Triple())
